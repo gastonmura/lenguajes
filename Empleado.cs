33f6d340594879e318c65d2f3282335bc1ca2ced
Gaston Mura <gastonmura@hotmail.com>
2013-06-12 08:51:02 -0300

    IEnumerator Icomparable
    Ejemplos de usos de estas interfaces en C#

---
+++ Empleado.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Empleados
+{
+
+    public class ExceptionDNI : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "El DNI no puede ser cero";
+            }
+        }
+        
+    }
+    public abstract class Empleado: IComparable
+    {
+        private DateTime fechaIngreso;
+        private int horasTrabajadas;
+        private string nombre;
+        private string dni;
+
+        private delegate int eComp (Empleado x, Empleado y);
+
+        private static eComp _comp = new eComp(Empleado.CompareNombre);
+
+        private static int CompareNombre(Empleado x, Empleado y)
+        {
+              return x.Nombre.CompareTo(y.Nombre);
+        }
+                       
+        private static int CompareDni(Empleado x, Empleado y) 
+        {
+               return x.Dni.CompareTo(y.Dni);
+        }
+
+        
+        
+        public int CompareTo(object obj)
+        {
+            return Empleado._comp((Empleado)this, (Empleado)obj); ;
+        }
+
+        public static void setCompareDni()
+        {
+            Empleado._comp = Empleado.CompareDni;
+        }
+
+        public static void setCompareNombre()
+        {
+            Empleado._comp = Empleado.CompareNombre;
+        }
+
+
+        public string Nombre {
+            get {return this.nombre;}
+            set { this.nombre = value; }
+        }
+
+        public string Dni {
+            get {return this.dni;}
+            set {
+                if (value == "0")
+                    throw new ExceptionDNI();
+                this.dni = value; }
+        }
+
+        public DateTime FechaIngreso
+        {
+            get
+            {
+                return this.fechaIngreso;
+
+            }
+            set
+            {
+                this.fechaIngreso = value;
+            }
+        }
+
+        public int HorasTrabajadas
+        {
+            get
+            {
+                return this.horasTrabajadas;
+            }
+            set
+            {
+                this.horasTrabajadas = value;
+            }
+        }
+
+        public abstract double DescuentoObraSocial();
+
+        public abstract double BonoAntigüedad();
+
+        public abstract double SueldoBasico();
+
+        public double AsignacionFamiliar()
+        {
+            return 30;
+        }
+        public double Sueldo()
+        {
+            return this.SueldoBasico() + this.AsignacionFamiliar() + this.BonoAntigüedad() - this.DescuentoObraSocial();
+        }
+
+        public Empleado(int horas, DateTime fechaIngreso)
+        {
+            this.HorasTrabajadas = horas;
+            this.FechaIngreso = fechaIngreso;
+        }
+
+        public Empleado(string n, string d) {
+            this.Nombre = n;
+            this.Dni = d;
+        }
+
+        public Empleado()
+        {
+
+        }
+        
+
+    }
+
+}
---
+++ Empresa.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Empleados
+{
+
+    class EmpresaEnumerator : IEnumerator
+    {
+        private int actual;
+        private Empresa e;
+
+        public EmpresaEnumerator(Empresa e)
+        {
+            actual = -1;
+            this.e = e;
+        }
+        public Object Current
+        {
+            get { return this.e[this.actual]; }
+        }
+
+        public bool MoveNext()
+        {
+            if (this.actual >= this.e.CantidadEmpleados() - 1)
+                return false;
+            this.actual++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            this.actual = -1;
+        }
+    }
+
+    class Empresa: IEnumerable
+    {
+        private System.Collections.Generic.List<Empleado> empleados;
+             
+        public Empresa()
+        { 
+            this.empleados = new List<Empleado>();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new EmpresaEnumerator(this);
+        }
+
+        public int CantidadEmpleados()
+        {
+            return this.empleados.Count;
+        }
+
+        public Empleado this[int index]
+        {
+            get { return this.empleados[index]; }
+        }
+
+        public void AgregarEmpleado(Empleado e) 
+        {
+            this.empleados.Add(e);
+        }
+    }
+}
